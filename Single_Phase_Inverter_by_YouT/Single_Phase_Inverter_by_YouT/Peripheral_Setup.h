b96c2ffac6ede25512e583d736fc3688934fe474
ANTU ROY <vandematram15aug1947@gmail.com>
2025-05-21 21:48:40 +0530

    _base Commit.

--- Single_Phase_Inverter_by_YouT/Single_Phase_Inverter_by_YouT/Peripheral_Setup.h
+++ Single_Phase_Inverter_by_YouT/Single_Phase_Inverter_by_YouT/Peripheral_Setup.h
@@ -1,15 +1,36 @@
 /*
  * Peripheral_Setup.h
  *
- Created on: 10 Nov 2024
- *      Author: Vande
+ * Created on: 10 Nov 2024
+ * Author: Vande
  */
 
-#ifndef PERIPHERAL_SETUP_H_
+#ifndef PERIPHERAL_SETUP_H_    /* Include guard to prevent multiple inclusion */
 #define PERIPHERAL_SETUP_H_
+
+/* Include necessary header for device-specific definitions */
 #include "F28x_Project.h"
 
+/* Function Prototypes */
+
+/*
+ * Function: Setup_GPIO
+ * --------------------
+ * Configures GPIO pins for:
+ *  - LED output on GPIO31 (Port A) and GPIO34 (Port B)
+ *  - ePWM output pins:
+ *      - GPIO0 and GPIO1 for ePWM1A and ePWM1B
+ *      - GPIO157 and GPIO159 for ePWM7A and ePWM8A
+ */
 void Setup_GPIO(void);
+
+/*
+ * Function: Setup_ePWM
+ * ---------------------
+ * Initializes and configures:
+ *  - ePWM1 module for center-aligned PWM generation with dead-band
+ *  - ePWM7 and ePWM8 modules for phase-shifted PWM generation
+ */
 void Setup_ePWM(void);
 
 #endif /* PERIPHERAL_SETUP_H_ */
--- Single_Phase_Inverter_by_YouT/Single_Phase_Inverter_by_YouT/Single_Phase_Inverter_by_YouT.c
+++ Single_Phase_Inverter_by_YouT/Single_Phase_Inverter_by_YouT/Single_Phase_Inverter_by_YouT.c
@@ -1,73 +1,95 @@
 /*
  * Single_Phase_Inverter_by_YouT.c
  *
- Created on: 10 Nov 2024
- *      Author: Vande
+ * Created on: 10 Nov 2024
+ * Author: Vande
  */
 
 #include "Peripheral_Setup.h"
 #include "math.h"
 
-/**
- * main.c
- */
+/* Global Variables */
 uint32_t count = 0, index = 0;
 uint16_t sinetable[125];
 
+/* Function Prototypes */
 __interrupt void isr_cpu_timer0(void);
 
-int main(void){
-	InitSysCtrl();							// Initialize System Control:
+/*
+ * Function: main
+ * --------------
+ * Entry point of the program. Initializes system control, peripherals,
+ * CPU timer and sine table, then enters an infinite loop toggling GPIOs.
+ */
+int main(void)
+{
+    InitSysCtrl();  /* Initialize System Control: PLL, WatchDog, enable Peripheral Clocks */
 
-	EALLOW;
-	CpuSysRegs.PCLKCR0.bit.CPUTIMER0 = 1;
-	EDIS;
+    EALLOW;
+    CpuSysRegs.PCLKCR0.bit.CPUTIMER0 = 1;  /* Enable clock for CPU Timer 0 */
+    EDIS;
 
-	DINT;									// Disable CPU interrupts
-	InitPieCtrl();							// Initialize the PIE control registers to their default state
-	IER = 0x0000;							// Disable CPU interrupts
-	IFR = 0x0000;							// Clear all CPU interrupt flags:
-	InitPieVectTable();						// Initialize the PIE vector table
+    DINT;              /* Disable CPU interrupts */
+    InitPieCtrl();     /* Initialize PIE control registers to default */
+    IER = 0x0000;      /* Disable all CPU interrupts */
+    IFR = 0x0000;      /* Clear all CPU interrupt flags */
+    InitPieVectTable();/* Initialize the PIE vector table */
 
-	Setup_GPIO();
-	Setup_ePWM();
+    Setup_GPIO();      /* Setup GPIO for LEDs and ePWM outputs */
+    Setup_ePWM();      /* Configure ePWM modules */
 
-	EALLOW;
-	PieVectTable.TIMER0_INT = &isr_cpu_timer0;
-	PieCtrlRegs.PIEIER1.bit.INTx7 = 1; 		//Timer 0
-	EDIS;
-	IER |= M_INT1;
+    EALLOW;
+    PieVectTable.TIMER0_INT = &isr_cpu_timer0;     /* Map Timer0 ISR */
+    PieCtrlRegs.PIEIER1.bit.INTx7 = 1;             /* Enable INTx7 for Timer 0 */
+    EDIS;
+    IER |= M_INT1;      /* Enable CPU INT1 */
 
-	InitCpuTimers();
-	ConfigCpuTimer(&CpuTimer0, 200, 160);
-	CpuTimer0Regs.TCR.all = 0x4001;
+    InitCpuTimers();    /* Initialize CPU Timers */
+    ConfigCpuTimer(&CpuTimer0, 200, 160); /* 200MHz SYSCLK, 160us Period */
+    CpuTimer0Regs.TCR.all = 0x4001;       /* Enable Timer0 with interrupt */
 
-	for(index = 0; index < 125; index++){
-		sinetable[index] = (uint16_t)(1000.0*(1.0 + sin(6.28318531/125.0*((float)index))));
-	}
-	index = 0;
+    /* Generate sine lookup table */
+    for(index = 0; index < 125; index++)
+    {
+        sinetable[index] = (uint16_t)(1000.0 * (1.0 + sin(6.28318531 / 125.0 * ((float)index))));
+    }
+    index = 0;
 
-	EINT;  									// Enable Global interrupt INTM
-	ERTM;  									// Enable Global realtime interrupt DBGM
-	GpioDataRegs.GPBDAT.bit.GPIO34 = 1;
-	GpioDataRegs.GPADAT.bit.GPIO31 = 0;
-	while(1){
-		for(count = 0; count < 0x00FFFFFF; count++){
+    EINT;  /* Enable global interrupt INTM */
+    ERTM;  /* Enable real-time interrupt DBGM */
 
-		}
-		GpioDataRegs.GPBTOGGLE.bit.GPIO34 = 1;
-		//GpioDataRegs.GPATOGGLE.bit.GPIO31 = 1;
-	}
-	return 0;
+    GpioDataRegs.GPBDAT.bit.GPIO34 = 1;  /* Set GPIO34 HIGH */
+    GpioDataRegs.GPADAT.bit.GPIO31 = 0;  /* Set GPIO31 LOW */
+
+    /* Main loop: simple delay + LED toggle */
+    while(1)
+    {
+        for(count = 0; count < 0x00FFFFFF; count++)
+        {
+            /* Software delay */
+        }
+
+        GpioDataRegs.GPBTOGGLE.bit.GPIO34 = 1;  /* Toggle GPIO34 */
+        GpioDataRegs.GPATOGGLE.bit.GPIO31 = 1;  /* Toggle GPIO31 */
+    }
+
+    return 0;
 }
 
-__interrupt void isr_cpu_timer0(void){
-	GpioDataRegs.GPATOGGLE.bit.GPIO31 = 1;
+/*
+ * Function: isr_cpu_timer0
+ * ------------------------
+ * Interrupt Service Routine for CPU Timer 0.
+ * Toggles GPIO31, updates index and sets PWM compare values using the sine table.
+ */
+__interrupt void isr_cpu_timer0(void)
+{
+    GpioDataRegs.GPATOGGLE.bit.GPIO31 = 1;  /* Toggle GPIO31 */
 
-	index = (index == 124) ? 0 : (index+1);
+    index = (index == 124) ? 0 : (index + 1);  /* Increment or wrap index */
 
-	EPwm7Regs.CMPA.bit.CMPA = sinetable[index];
-	EPwm8Regs.CMPA.bit.CMPA = sinetable[index];
+    EPwm7Regs.CMPA.bit.CMPA = sinetable[index];  /* Update ePWM7 compare value */
+    EPwm8Regs.CMPA.bit.CMPA = sinetable[index];  /* Update ePWM8 compare value */
 
-	PieCtrlRegs.PIEACK.all = PIEACK_GROUP1;
+    PieCtrlRegs.PIEACK.all = PIEACK_GROUP1;  /* Acknowledge interrupt */
 }
